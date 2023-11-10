a62fc3e4f4b29ce5c7d4a47cd20f4c7c68edbf64
elkhanismayil <elxan310@gmail.com>
2023-11-10 20:48:33 +0400

    some classes are updated

--- src/main/java/com/interntask/bookstore/config/SecurityConfig.java
+++ src/main/java/com/interntask/bookstore/config/SecurityConfig.java
@@ -4,17 +4,25 @@ import com.interntask.bookstore.security.CustomBasicAuthenticationEntryPoint;
 import com.interntask.bookstore.security.CustomBearerTokenAccessDeniedHandler;
 import com.interntask.bookstore.security.CustomBearerTokenAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+
+import static org.springframework.security.oauth2.jwt.JwtClaimNames.AUD;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +30,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
     private final CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
     private final CustomBearerTokenAuthenticationEntryPoint customBearerTokenAuthenticationEntryPoint;
     private final CustomBearerTokenAccessDeniedHandler customBearerTokenAccessDeniedHandler;
+
+    @Value("${app.jwt.location}")
+    private RSAPublicKey key;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -38,16 +48,34 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .httpBasic(httpBasic ->
-                        httpBasic.authenticationEntryPoint(customBasicAuthenticationEntryPoint))
                 .oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer.jwt(Customizer.withDefaults())
                                 .authenticationEntryPoint(customBearerTokenAuthenticationEntryPoint)
+                                .authenticationEntryPoint(customBasicAuthenticationEntryPoint)
                                 .accessDeniedHandler(customBearerTokenAccessDeniedHandler))
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        final NimbusJwtDecoder decoder = NimbusJwtDecoder.withPublicKey(this.key)
+                .build();
+        decoder.setJwtValidator(tokenValidator());
+        return decoder;
+    }
+
+    private OAuth2TokenValidator<Jwt> tokenValidator() {
+        final List<OAuth2TokenValidator<Jwt>> validators =
+                List.of(new JwtTimestampValidator(),
+                        new JwtIssuerValidator("http://foobar.com"),
+                        audienceValidator());
+        return new DelegatingOAuth2TokenValidator<>(validators);
+    }
+
+    private OAuth2TokenValidator<Jwt> audienceValidator() {
+        return new JwtClaimValidator<List<String>>(AUD, aud -> aud.contains("foobar"));
     }
 }
--- src/main/java/com/interntask/bookstore/security/CustomBearerTokenAuthenticationEntryPoint.java
+++ src/main/java/com/interntask/bookstore/security/CustomBearerTokenAuthenticationEntryPoint.java
@@ -1,9 +1,12 @@
 package com.interntask.bookstore.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interntask.bookstore.model.response.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,26 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import java.io.IOException;
 @Component
 public class CustomBearerTokenAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final HandlerExceptionResolver resolver;
+//    private final HandlerExceptionResolver resolver;
 
-    public CustomBearerTokenAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-        this.resolver = resolver;
-    }
+//    public CustomBearerTokenAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+//        this.resolver = resolver;
+//    }
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        this.resolver.resolveException(request, response, null, authException);
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        if (request.getAttribute("exception") != null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Expired JWT token");
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("JWT has expired")
+                .status(HttpStatus.UNAUTHORIZED)
+                .build();
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
